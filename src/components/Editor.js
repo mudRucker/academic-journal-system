12c7b7e0418fbbc3b24c98203d1c8d11023f2703
mudRucker <themudrucker@gmail.com>
2020-04-28 01:31:12 -0600

    Editor updated

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -12,7 +12,23 @@ class Editor extends React.Component {
     return (
       <div>
         <div style={{width: "700px", textAlign: "center", margin: "100px auto"}}>
-          Hey there. What do you want?
+          <br/>
+          (try to make all of these SQl calls right when the component first loads... use that special method)
+          <br/>
+          <br/>
+          __X Articles need Reviewers assigned to them__
+          <br/>
+          __Y Articles need to have a deadline set__
+          <br/>
+          __Z Articles require your editorial decision__
+          <br/>
+          __W Articles have a removal request from their author__
+          <br/>
+          <br/>
+          __Publish a completed academic journal__
+          <br/>
+          <br/>
+          (cody: make sure this guy has a special view of the article pages that lets him edit everything about it)
         </div>
       </div>
       
