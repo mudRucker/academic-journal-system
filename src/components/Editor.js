fd948311ab80e06e8509804154e62eceeec18a91
mudRucker <themudrucker@gmail.com>
2020-04-30 19:04:43 -0600

    Editor updated (stats calculation almost finished)

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,19 +1,18 @@
 import React from 'react';
 
 
-
 class Editor extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      allQueriesComplete: false
     };
     this.submissions = {
       data: null,
-      needingRevs: -1,
-      needingDeadline: -1,
-      needingDecision: -1,
-      withRemovalRequests: -1
+      needingRevs: [],
+      needingDeadline: [],
+      needingDecision: [],
+      withRemovalRequests: []
     }
     this.reviews = null;
     this.nominated = null;
@@ -38,64 +37,129 @@ class Editor extends React.Component {
         console.log("server response for SUBMISSION query: \n");
         console.log(data);
         this.submissions.data = data;
-        this.calcStats();
-      });
-    });
+        this.addRevsAttributeToSubs();
 
-    // retrieve all reviewer records
-    myQuery = "SELECT * FROM REVIEWS";
-    fetch('http://localhost:9000/select', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({query: myQuery})   
-    }).then(response => {
-      response.json().then(data => {
-        console.log("server response for REVIEWS query: \n");
-        console.log(data);
-        this.reviews = data;
-      });
-    });
+        // retrieve all reviewer records
+        myQuery = "SELECT * FROM REVIEWS";
+        fetch('http://localhost:9000/select', {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({query: myQuery})   
+        }).then(response => {
+          response.json().then(data => {
+            console.log("server response for REVIEWS query: \n");
+            console.log(data);
+            this.reviews = data;
 
-    // retrieve all reviewer nominations
-    myQuery = "SELECT * FROM NOMINATED";
-    fetch('http://localhost:9000/select', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({query: myQuery})   
-    }).then(response => {
-      response.json().then(data => {
-        console.log("server response for NOMINATED query: \n");
-        console.log(data);
-        this.nominated = data;
-      });
-    });
+            // add the rev to the sub they belong to
+            this.reviews.forEach((rev, revIndex, revArr) => {
+              this.submissions.data.forEach((sub, subIndex, subArr) => {
+                if (rev.subID === sub.subID)
+                  sub.revs.push(rev);
+              });
+            });
 
-    // retrieve all review requests
-    myQuery = "SELECT * FROM REQUESTS";
-    fetch('http://localhost:9000/select', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({query: myQuery})   
-    }).then(response => {
-      response.json().then(data => {
-        console.log("server response for REQUESTS query: \n");
-        console.log(data);
-        this.requests = data;
+            this.calcSubStats();
+
+            // BLAH TRYING to do it more efficiently (got confusing, so do it later. Brute force for now ^)
+            // trying to keep track of a found review's submission's index in the this.submissions.data array so 
+            // that a review for the same submission wouldn't need to search for that submission agian...abs
+
+            // let thisRevsSubIndex = {};
+            // this.reviews.forEach((rev, revIndex, revArr) => {
+            //   if (eval('thisRevsSubIndex.s' + rev.subID + ' === undefined')) {
+            //     this.submissions.data.forEach((sub, subIndex, subArr) => {
+            //       if (rev.subID === sub.subID)
+            //         eval('thisRevsSubIndex.s' + rev.subID + '=' + subIndex);
+            //       // more shit...
+            //     });
+            //   }
+            //   
+            
+            if (this.checkIfAllQueriesComplete()) 
+              this.setState({allQueriesComplete: true});
+          });
+        });
+
+        // retrieve all reviewer nominations
+        myQuery = "SELECT * FROM NOMINATED";
+        fetch('http://localhost:9000/select', {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({query: myQuery})   
+        }).then(response => {
+          response.json().then(data => {
+            console.log("server response for NOMINATED query: \n");
+            console.log(data);
+            this.nominated = data;
+
+
+            if(this.checkIfAllQueriesComplete()) 
+              this.setState({allQueriesComplete: true});
+          });
+        });
+
+        // retrieve all review requests
+        myQuery = "SELECT * FROM REQUESTS";
+        fetch('http://localhost:9000/select', {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({query: myQuery})   
+        }).then(response => {
+          response.json().then(data => {
+            console.log("server response for REQUESTS query: \n");
+            console.log(data);
+            this.requests = data;
+
+
+            if(this.checkIfAllQueriesComplete()) 
+              this.setState({allQueriesComplete: true});
+          });
+        });
+
       });
     });
   }
 
 
 
-  calcStats = () => {
-    this.submissions.needingRevs = 0;
-    this.submissions.needingDeadline = 0;
-    this.submissions.needingDecision = 0;
-    this.submissions.withRemovalRequests = 0;
+  addRevsAttributeToSubs = () => {
+    this.submissions.data.forEach((sub, i, arr) => { sub.revs = [] });
+  }
 
-    this.submissions.data.forEach((item, index, arr) => {
-      console.log(item.title);  // BOOKMARK. perform your actual stats calculations here
-    });                         // (and not this dummy, test one)
+
+
+  calcSubStats = () => {
+    this.submissions.data.forEach((sub, index, arr) => {
+
+      if (sub.revs.length < 3)  
+        this.submissions.needingRevs.push(index);
+      else
+        console.log(`nice: Submission ID '${sub.subID}' has 3+ reviewers already!`); 
+
+      if (sub.revDeadline === null)
+        this.submissions.needingDeadline.push(index);
+
+      if (sub.status === "Author requests removal")
+        this.submissions.withRemovalRequests.push(index);
+
+      if (sub.revs.length > 4)
+        alert("shit: submission ID " + sub.subID + " has " + 
+              sub.revs.length + " reviewers assigned to it.");        
+    });
+
+  }
+
+
+
+  checkIfAllQueriesComplete = () => {
+    if(this.submissions.data != null &&
+       this.reviews != null &&
+       this.nominated != null &&
+       this.requests != null)
+      return true;
+
+    return false;
   }
 
 
@@ -105,13 +169,16 @@ class Editor extends React.Component {
       <div>
         <div style={{width: "700px", textAlign: "center", margin: "100px auto"}}>
           <br/>
-          __X Articles need Reviewers assigned to them__
+          {this.submissions.needingRevs.length} Articles need Reviewers assigned to them
           <br/>
-          __Y Articles need to have a deadline set__
+          {this.submissions.needingDeadline.length} Articles need a deadline set
           <br/>
-          __Z Articles require your editorial decision__
+          {this.submissions.withRemovalRequests.length} Articles have removal requests
           <br/>
-          __W Articles have a removal request from their author__
+          <br/>
+          __Z Articles require your editorial decision__ (how do you know this?)  BOOKMARK
+          <br/>
+          <br/>
           <br/>
           <br/>
           __Publish a completed academic journal__
